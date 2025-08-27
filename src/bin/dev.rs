f3441b5467ace53beae8aa92fb98632ea49fdc48
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2025-08-27 17:31:12 -0400

    chore: update dev

--- src/bin/dev.rs
+++ src/bin/dev.rs
@@ -26,18 +26,27 @@ void main() {
 "#;
 
     let mut material = Material::new(vertex_shader_source, fragment_shader_source);
-    material.set_uniform("color", Uniform::Vec4([0.0, 1.0, 0.0, 1.0]));
 
-    let mut transform = Transform::new();
     let geometry = Geometry::quad();
     let mut renderer = Renderer::new();
 
-    let rotation = Quat::from_rotation_z(0.01);
+    let mut transform1 = Transform::new();
+    let rotation1 = Quat::from_rotation_z(0.01);
+
+    let mut transform2 = Transform::new();
+    let rotation2 = Quat::from_rotation_z(0.02);
 
     let callback = Closure::wrap(Box::new(move || {
-        transform.rotation = transform.rotation.mul_quat(rotation);
-        material.set_uniform("transform", Uniform::Mat4(transform.to_array()));
+        renderer.clear();
 
+        transform1.rotation = transform1.rotation.mul_quat(rotation1);
+        material.set_uniform("transform", Uniform::Mat4(transform1.to_array()));
+        material.set_uniform("color", Uniform::Vec4([0.0, 1.0, 0.0, 1.0]));
+        renderer.render(&material, &geometry);
+
+        transform2.rotation = transform2.rotation.mul_quat(rotation2);
+        material.set_uniform("transform", Uniform::Mat4(transform2.to_array()));
+        material.set_uniform("color", Uniform::Vec4([1.0, 0.0, 0.0, 1.0]));
         renderer.render(&material, &geometry);
     }) as Box<dyn FnMut()>);
 
